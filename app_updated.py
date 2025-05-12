982eed59feb47aab437ae179830561e0127b47f4
kimrabago <rabagokim26@gmail.com>
2025-05-13 01:38:23 +0800

    updateddddddddd

--- app_updated.py
+++ app_updated.py
@@ -25,7 +25,7 @@ try:
         "type": os.getenv("TYPE"),
         "project_id": os.getenv("PROJECT_ID"),
         "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-        "private_key": os.getenv("PRIVATE_KEY"),
+        "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),
         "client_email": os.getenv("CLIENT_EMAIL"),
         "client_id": os.getenv("CLIENT_ID"),
         "auth_uri": os.getenv("AUTH_URI", "https://accounts.google.com/o/oauth2/auth"),
